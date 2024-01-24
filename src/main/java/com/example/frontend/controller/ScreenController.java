37c50c0dacdf21cab9d2aeea9516bdeb0240e2d2
peter <peti.tavaszi@gmail.com>
2024-01-24 14:10:42 +0100

    renamed question_upload.fxml to question_create.fxml

--- src/main/java/com/example/frontend/controller/ScreenController.java
+++ src/main/java/com/example/frontend/controller/ScreenController.java
@@ -12,7 +12,7 @@ public abstract class ScreenController {
 
     public static Screen<CreateAutomatic_ScreenController> createTestAutomatic = new Screen<>("sites/create_automatic.fxml");
     public static Screen<CreateManual_ScreenController> createTestManual = new Screen<>("sites/create_manual.fxml");
-    public static Screen<QuestionUpload_ScreenController> questionUpload = new Screen<>("sites/question_upload.fxml");
+    public static Screen<QuestionUpload_ScreenController> questionUpload = new Screen<>("sites/question_create.fxml");
     public static Screen<QuestionEdit_ScreenController> questionEdit = new Screen<>("sites/question_edit.fxml");
     public static Screen<QuestionEdit_ScreenController> addStudyProgram = new Screen<>("sites/add_studyProgram.fxml");
 
